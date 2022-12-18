2702a19b753d261dd67b8c012a894860d59383c7
JoelValor <120955446+JoelValor@users.noreply.github.com>
2022-12-18 17:15:58 +0100

    Add files via upload

---
+++ web.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : localhost
+-- Généré le : dim. 18 déc. 2022 à 17:15
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `web`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `reservas`
+--
+-- Erreur de lecture de structure pour la table web.reservas : #1142 - La commande &#039;SHOW&#039; est interdite à l&#039;utilisateur: &#039;&#039;@&#039;localhost&#039; sur la table &#039;reservas&#039;
+-- Erreur de lecture des données pour la table web.reservas : #1064 - Erreur de syntaxe près de &#039;FROM `web`.`reservas`&#039; à la ligne 1
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `usuarios`
+--
+-- Erreur de lecture de structure pour la table web.usuarios : #1142 - La commande &#039;SHOW&#039; est interdite à l&#039;utilisateur: &#039;&#039;@&#039;localhost&#039; sur la table &#039;usuarios&#039;
+-- Erreur de lecture des données pour la table web.usuarios : #1064 - Erreur de syntaxe près de &#039;FROM `web`.`usuarios`&#039; à la ligne 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
